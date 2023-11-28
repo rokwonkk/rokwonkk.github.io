9764e91d08d574a86739c09a5c06cb29aee47fde
rokwonkk <jirisan8147@gmail.com>
2023-11-29 03:10:26 +0900

    111

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701193406';
+const cacheName = 'chirpy-1701194941';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
