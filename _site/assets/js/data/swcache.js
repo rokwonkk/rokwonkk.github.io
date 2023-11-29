250f221fc2b4e596ed117b2d4e71743135ca021c
rokwonkk <jirisan8147@gmail.com>
2023-11-29 23:14:15 +0900

    commit

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -38,6 +38,9 @@ const resource = [
 /* The request url with below domain will be cached */
 const allowedDomains = [
   
+    'www.googletagmanager.com',
+    'www.google-analytics.com',
+  
 
   'localhost:4000',
 
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701256748';
+const cacheName = 'chirpy-1701267114';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
